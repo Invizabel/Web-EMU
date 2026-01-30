d133d11d2c32c251b92ffec6055de417a0483095
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-01-29 22:00:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -88,5 +88,5 @@ for i in textures_previews:
     with open(f"assets/textures/previews/{i}.png","wb") as file:
         file.write(response)
 
-os.system("sudo cp -r assets /var/www/html/")
+os.system("sudo cp -r assets/ /var/www/html/")
 os.system("sudo cp -r * /var/www/html/classic/")
